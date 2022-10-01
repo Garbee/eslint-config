ae881f5538ee7f7a7b00fac6aa38c09e33f9946a
Jonathan Garbee <jonathan@garbee.me>
2022-10-01 14:41:20 -0400

    feat: add anti-trojan-source plugin
    Signed-off-by: Jonathan Garbee <jonathan@garbee.me>

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 
 const baseRules = require('./rules/eslint-base.js');
 const commentRules = require('./rules/eslint-comments.js');
+const {error} = require('./rules/options.js');
 const unicornRules = require('./rules/unicorn.js');
 
 module.exports = {
   plugins: [
     'eslint-comments',
     'unicorn',
+    'anti-trojan-source',
   ],
   rules: {
     ...baseRules,
     ...commentRules,
     ...unicornRules,
+    'anti-trojan-source/no-bidi': error,
   },
 };
