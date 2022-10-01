2025eba4d9e1851abc0c3d80d9e2e8e02b2ee25c
Jonathan Garbee <jonathan@garbee.me>
2022-10-01 14:39:56 -0400

    feat: add unicorn rules
    Signed-off-by: Jonathan Garbee <jonathan@garbee.me>

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 const baseRules = require('./rules/eslint-base.js');
 const commentRules = require('./rules/eslint-comments.js');
+const unicornRules = require('./rules/unicorn.js');
 
 module.exports = {
-  plugins: ['eslint-comments'],
+  plugins: [
+    'eslint-comments',
+    'unicorn',
+  ],
   rules: {
     ...baseRules,
     ...commentRules,
+    ...unicornRules,
   },
 };
