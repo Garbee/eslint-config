e58b3a3fe13f48df1d3dc453b8cc233d76432570
Jonathan Garbee <jonathan@garbee.me>
2022-10-03 08:49:58 -0400

    feat: order framework lifecycles
    Add initial support for lit, angular, and web component
    lifecycle events.
    
    Signed-off-by: Jonathan Garbee <jonathan@garbee.me>

--- rules/eslint/sort-class-members.js
+++ rules/eslint/sort-class-members.js
@@ -1,9 +1,65 @@
 const {error} = require('./options.js');
 
+const customElementLifecycleHooks = [
+  'connectedCallback',
+  'disconnectedCallback',
+  'attributeChangedCallback',
+  'adoptedCallback',
+].map((hook) => {
+  return {
+    name: hook,
+    type: 'method',
+  };
+});
+
+const litLifecycleHooks = [
+  // Update Cycle
+  'shouldUpdate',
+  'willUpdate',
+  'update',
+  'render',
+
+  // Post-Update
+  'firstUpdated',
+  'updated',
+].map((hook) => {
+  return {
+    name: hook,
+    type: 'method',
+  };
+});
+
+const angularLifecycleHooks = [
+  'ngOnChanges',
+  'ngOnInit',
+  'ngDoCheck',
+  'ngAfterContentInit',
+  'ngAfterContentChecked',
+  'ngAfterViewInit',
+  'ngAfterViewChecked',
+  'ngOnDestroy',
+].map((hook) => {
+  return {
+    name: hook,
+    type: 'method',
+  };
+});
+
+const frameworkLifecycles = [
+  '[webcomponent-lifecycle]',
+  '[lit-lifecycle]',
+  '[angular-lifecycle]',
+];
+
 module.exports = {
   'sort-class-members/sort-class-members': [
     error,
     {
+      groups: {
+        'angular-lifecycle': angularLifecycleHooks,
+        'lit-lifecycle': litLifecycleHooks,
+        'webcomponent-lifecycle': customElementLifecycleHooks,
+      },
       order: [
         '[static-properties]',
         '[static-methods]',
@@ -14,6 +70,7 @@ module.exports = {
         '[getters]',
         '[setters]',
         'constructor',
+        ...frameworkLifecycles,
         '[methods]',
         '[conventional-private-methods]',
         '[everything-else]',
